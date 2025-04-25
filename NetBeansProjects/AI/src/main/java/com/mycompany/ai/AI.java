f98a1d6dbb69d98277e2e427984c4bb49351c415
Samia Asmar <asmarsamia2003@gmail.com>
2025-04-25 22:19:48 +0300

    Graph Coloring with Simulated Annealing and interactive GUI support

---
+++ NetBeansProjects/AI/src/main/java/com/mycompany/ai/AI.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ai;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author samiaasmar
+ */
+public class AI {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920, 1080); 
+        frame.setLocationRelativeTo(null); 
+        frame.setVisible(true); }
+    
+}
+
